f1801b0fca267eb136603d449570d674ba4cb8c8
Luke Mansell <luke.mansell@gmail.com>
2022-07-21 12:41:28 +1200

    Shift files

---
+++ Stax.UseSwaggerAuthorization/Middleware/SwaggerMiddleware.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using UseSwaggerAuthorization.Models;
+
+namespace UseSwaggerAuthorization.Middleware;
+
+public class SwaggerMiddleware
+{
+    private readonly RequestDelegate _requestDelegate;
+    private readonly SwaggerBasicAuthorizationOptions _options;
+    
+    public SwaggerMiddleware(RequestDelegate requestDelegate, IOptions<SwaggerBasicAuthorizationOptions> options)
+    {
+        _requestDelegate = requestDelegate;
+        _options = options.Value;
+    }
+
+    public async Task InvokeAsync(HttpContext httpContext)
+    {
+        if (httpContext.Request.Path.StartsWithSegments("/swagger"))
+        {
+            var request = httpContext.Request;
+            var authorization = request.Headers["Authorization"].ToString();
+            if (authorization != null && authorization.StartsWith("basic", StringComparison.OrdinalIgnoreCase))
+            {
+                var header = AuthenticationHeaderValue.Parse(authorization);
+                var credentials = Encoding.UTF8.GetString(
+                    Convert.FromBase64String(header.Parameter ?? throw new InvalidOperationException())
+                    ).Split(':');
+                var username = credentials[0];
+                var password = credentials[1];
+                
+                if (username.Equals(_options.Username) && password.Equals(_options.Password)) {
+                    await _requestDelegate.Invoke(httpContext).ConfigureAwait(false);
+                    return;
+                }
+            }
+            httpContext.Response.Headers["WWW-Authenticate"] = "Basic";
+            httpContext.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+        }
+        else
+        {
+            await _requestDelegate.Invoke(httpContext).ConfigureAwait(false);
+        }
+    }
+
+}
---
+++ Stax.UseSwaggerAuthorization/Models/SwaggerAuthorizationOptions.cs
@@ -0,0 +1,13 @@
+﻿namespace UseSwaggerAuthorization.Models;
+
+/// <summary>
+/// Class which contains a username and password which can be used to protect a swagger endpoint
+/// </summary>
+public class SwaggerBasicAuthorizationOptions
+{
+    // Username value which you wish to use
+    public string Username { get; set; }
+
+    // Password value which you wish to use
+    public string Password { get; set; }
+}
