345469196d9ed1b9fff201eb9ae5153bdfeabafc
Luke Mansell <luke.mansell@gmail.com>
2022-07-21 12:39:50 +1200

    Initial work

---
+++ Stax.UseSwaggerAuthorization/Stax.UseSwaggerAuthorization/Extensions/SwaggerAuthorizationExtension.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Options;
+using UseSwaggerAuthorization.Middleware;
+using UseSwaggerAuthorization.Models;
+
+namespace UseSwaggerAuthorization.Extensions;
+
+public static class SwaggerAuthorizationExtension
+{
+    /// <summary>
+    /// A basic authorization middleware, which allows you to protect your swagger endpoint by a username and password.
+    /// </summary>
+    /// <param name="applicationBuilder"></param>
+    /// <param name="authorizationOptions">SwaggerBasicAuthorizationOptions which contains the username and password which
+    /// you want to use to protect the swagger endpoint.</param>
+    /// <returns></returns>
+    public static IApplicationBuilder UseSwaggerBasicAuthorization
+        (this IApplicationBuilder applicationBuilder, SwaggerBasicAuthorizationOptions authorizationOptions)
+    {
+        return applicationBuilder.UseMiddleware<SwaggerMiddleware>(Options.Create(authorizationOptions));
+    }
+
+    private static IApplicationBuilder UseSwaggerIAPAuthorization
+        (this IApplicationBuilder applicationBuilder)
+    {
+        throw new NotImplementedException();
+    }
+}
